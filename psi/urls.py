a7fb83fc2fd46163e5f8097a013a0b8865a955d2
RafaelStudartDiPiero <rafael.spiero@gmail.com>
2022-06-09 13:58:49 -0300

    Implemented Logging

--- psi/urls.py
+++ psi/urls.py
@@ -21,7 +21,8 @@ import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("pages.urls")),
+    path('accounts/', include('allauth.urls')),
+    path("", include("pages.urls", namespace="pages")),
     path("services/", include("services.urls")),
 ]
 
